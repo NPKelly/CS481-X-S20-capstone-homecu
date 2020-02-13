9f4823789edd3a1ac73b63f8bf56d1d650b739ce
NPKelly <kelly.nicholasp@gmail.com>
2020-02-13 12:15:33 -0700

    Initial Setup for the Monitor App in docker/django

--- code/api/api/settings.py
+++ code/api/api/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "Monitor/templates"),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
