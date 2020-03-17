5dca592ba19dd5f6b37a0eca6a22beccc4200114
Randy Camacho <43118312+RandyCamacho@users.noreply.github.com>
2020-03-17 03:27:20 -0600

    added newUser form

--- code/api/Monitor/urls.py
+++ code/api/Monitor/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
         path('', views.index, name='index'),
-        path('creditunions/',views.creditunions,name='creditunions')
+	path('creditunions/',views.creditunions,name='creditunions'),
+	path('newUser/', views.newUser,name='newUser'),
         ]
--- code/api/Monitor/views.py
+++ code/api/Monitor/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth import login
 from .forms import LoginForm
+from .forms import SignUpForm
+from django.contrib.auth import login, authenticate
+
 from .authentication_backend import AuthBackend
 from django.core import serializers
 from .models import BsuClientlist
@@ -35,6 +38,17 @@ def index(request):
     else:
         form = LoginForm()
     return render(request, 'index.html', {'form': form, 'errors': login_errors})
+
+def newUser(request):
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			username = form.cleaned_data['user_name']
+			password = form.cleaned_data['password']
+			return redirect('creditunions/')
+	else:
+		form = SignUpForm()
+	return render(request, 'newUser.html', {'form': form})
 
 def creditunions(request):
     return render(request, 'creditunions.html')
